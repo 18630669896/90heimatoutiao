b62762d8658cc452b459fc000ae7f20f5ea89394
xieman <2659945255@qq.com>
2019-12-19 10:38:15 +0800

    黑马头条PC-登录模块-页面布局及样式(完成)

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -4,19 +4,34 @@
       <div class="title">
         <img class="login-logo" src="../../../public/img/logo_index.png" alt />
       </div>
+      <!-- 表单 -->
+      <el-form>
+        <!-- 表单域 -->
+        <el-form-item>
+          <el-input placeholder="请输入手机号"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input style="width:68%" placeholder="请输入验证码"></el-input>
+          <el-button style="float:right" plain>获取验证码</el-button>
+        </el-form-item>
+        <el-form-item>
+          <el-checkbox>我已阅读并同意用户协议和隐私条款</el-checkbox>
+        </el-form-item>
+        <el-form-item>
+          <el-button style="width:100%" type="primary">登录</el-button>
+        </el-form-item>
+      </el-form>
     </el-card>
   </div>
 </template>
 
 <script>
 export default {
-  data () {
 
-  }
 }
 </script>
 
-<style lang = 'less' scoped>
+<style lang="less" scoped>
 .login {
   background-image: url("../../../public/img/back.jpg");
   height: 100vh;
@@ -30,6 +45,7 @@ export default {
     height: 350px;
     .title {
       text-align: center;
+      margin-bottom: 30px;
       img {
         height: 45px;
       }
