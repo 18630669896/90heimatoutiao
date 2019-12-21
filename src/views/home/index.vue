54e2340cefeac75684c321a69e085920c7c02266
xieman <2659945255@qq.com>
2019-12-21 10:34:53 +0800

    ## 黑马头条PC-主页模块-页面布局

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,5 +1,12 @@
 <template>
-  <div>主页</div>
+  <!-- 放置一个大容器 -->
+  <el-container>
+    <el-aside>左侧</el-aside>
+    <el-container>
+      <el-header>头部</el-header>
+      <el-main>主体</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
