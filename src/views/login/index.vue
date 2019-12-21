3463bb2d28f143c1311b93b14178f70b801fa8ca
xieman <2659945255@qq.com>
2019-12-21 09:22:24 +0800

    黑马头条PC-登录模块-数据绑定及校验

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -5,20 +5,20 @@
         <img class="login-logo" src="../../../public/img/logo_index.png" alt />
       </div>
       <!-- 表单 -->
-      <el-form>
+      <el-form ref="myForm" :model="loginForm" :rules="loginRules">
         <!-- 表单域 -->
-        <el-form-item>
-          <el-input placeholder="请输入手机号"></el-input>
+        <el-form-item prop="mobile">
+          <el-input v-model="loginForm.mobile" placeholder="请输入手机号"></el-input>
         </el-form-item>
-        <el-form-item>
-          <el-input style="width:68%" placeholder="请输入验证码"></el-input>
+        <el-form-item prop="code">
+          <el-input v-model="loginForm.code" style="width:68%" placeholder="请输入验证码"></el-input>
           <el-button style="float:right" plain>获取验证码</el-button>
         </el-form-item>
-        <el-form-item>
-          <el-checkbox>我已阅读并同意用户协议和隐私条款</el-checkbox>
+        <el-form-item prop="check">
+          <el-checkbox v-model="loginForm.check">我已阅读并同意用户协议和隐私条款</el-checkbox>
         </el-form-item>
         <el-form-item>
-          <el-button style="width:100%" type="primary">登录</el-button>
+          <el-button @click="submitForm" style="width:100%" type="primary">登录</el-button>
         </el-form-item>
       </el-form>
     </el-card>
@@ -27,7 +27,35 @@
 
 <script>
 export default {
-
+  data () {
+    return {
+      loginForm: {
+        mobile: '', // 手机号
+        code: '', // 验证码
+        check: false // 是否勾选同意按钮
+      },
+      loginRules: {
+        mobile: [{ required: true, message: '请输入您的手机号' }, { pattern: /^1[3456789]\d{9}$/, message: '手机号格式不正确' }],
+        code: [{ required: true, message: '请输入您的验证码' }, { pattern: /^\d{6}$/, message: '验证码格式不正确' }],
+        check: [{ validator: function (rule, value, callback) {
+          if (value) {
+            callback()
+          } else {
+            callback(new Error('请勾选同意被坑按钮'))
+          }
+        } }]
+      }
+    }
+  },
+  methods: {
+    submitForm () {
+      this.$refs.myForm.validate(function (isOK) {
+        if (isOK) {
+          console.log('验证通过，请调用接口')
+        }
+      })
+    }
+  }
 }
 </script>
 
