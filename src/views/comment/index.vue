74370955979bc1602f1aff4005dfb5d0382b551e
xieman <2659945255@qq.com>
2019-12-23 21:38:11 +0800

    黑马头条PC-评论列表-评论数据加载到页面及状态过滤

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -4,19 +4,50 @@
             <!-- 插槽内容 -->
             <template slot="title">评论管理</template>
         </bread-crumb>
-        <el-table>
-            <el-table-column width="500" label="标题"></el-table-column>
-            <el-table-column label="评论状态"></el-table-column>
-            <el-table-column label="总评论数"></el-table-column>
-            <el-table-column label="粉丝评论数"></el-table-column>
-            <el-table-column label="操作"></el-table-column>
+        <el-table :data="list">
+            <el-table-column prop="title" width="500" label="标题"></el-table-column>
+            <el-table-column :formatter="formatterBoolean" prop="comment_status" label="评论状态"></el-table-column>
+            <el-table-column prop="total_comment_count" label="总评论数"></el-table-column>
+            <el-table-column prop="fans_comment_count" label="粉丝评论数"></el-table-column>
+            <el-table-column label="操作">
+                <!-- 需要根据状态来判断是关闭还是打开 -->
+                <!-- 作用于插槽 -->
+                <template slot-scope="obj">
+                    <!-- {{obj.row}} -->
+                    <el-button size="small" type="text">修改</el-button>
+                    <el-button size="small" type="text">{{obj.row.comment_status?'关闭':'打开'}}评论</el-button>
+                </template>
+
+            </el-table-column>
         </el-table>
     </el-card>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      list: []
+    }
+  },
+  methods: {
+    getComment () {
+      this.$axios({
+        url: '/articles',
+        params: {
+          response_type: 'comment'
+        }
+      }).then(result => {
+        this.list = result.data.results
+      })
+    },
+    formatterBoolean (row, column, cellValue, index) {
+      return cellValue ? '正常' : '关闭'
+    }
+  },
+  created () {
+    this.getComment() // 获取数据
+  }
 }
 </script>
 
