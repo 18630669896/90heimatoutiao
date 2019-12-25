660a6d6b573952116f25b20e52fe326e0a8afb66
xieman <2659945255@qq.com>
2019-12-25 11:03:43 +0800

    黑马头条PC-素材列表-分页组件及请求---黑马头条PC-素材列表-素材的收藏和删除

--- src/views/material/index.vue
+++ src/views/material/index.vue
@@ -11,7 +11,7 @@
                         <el-card :body-style="{ padding: '0px' }" class="img-card" v-for="item in list" :key="item.id">
                             <img :src="item.url" alt="">
                             <el-row type="flex" justify="space-around" style="height:40px; font-size:18px;background-color:#f4f5f6" align="middle">
-                                <i class="el-icon-star-on"></i>
+                                <i @click="collectOrCollect(item)" :style="{color:item. is_collected?'red':'#000'}" class="el-icon-star-on"></i>
                                 <i class="el-icon-delete"></i>
                             </el-row>
                         </el-card>
@@ -28,7 +28,16 @@
             </el-tabs>
             <!-- <el-button type="primary">上传图片</el-button> -->
         </el-row>
-
+        <el-row type="flex" justify="center">
+          <el-pagination
+            @current-change="changePage"
+            background
+            layout="prev, pager, next"
+            :page-size="page.pageSize"
+            :current-page="page.currentPage"
+            :total="page.total">
+          </el-pagination>
+        </el-row>
     </el-card>
 </template>
 
@@ -37,21 +46,49 @@ export default {
   data () {
     return {
       activeName: 'all', // 当前选中的标签
-      list: [] // 用于接收图片数据
+      list: [], // 用于接收图片数据
+      page: {
+        pageSize: 8, // 每页显示的数据
+        total: 0, // 总条数
+        currentPage: 1
+
+      }
     }
   },
   methods: {
+    // 收藏或者取消收藏
+    collectOrCollect (item) {
+      this.$axios({
+        url: `/user/images/${item.id}`,
+        method: 'put',
+        data: {
+          collect: !item.is_collected // 如果已收藏那么就取消收藏，如果未那么点击收藏
+        }
+      }).then(result => {
+        this.getMaterial()
+      })
+    },
+    // 点击换页
+    changePage (newPage) {
+      this.page.currentPage = newPage
+      this.getMaterial()
+    },
+    // 切换tab栏
     changeTab () {
+      this.page.currentPage = 1
       this.getMaterial()
     },
     getMaterial () {
       this.$axios({
         url: '/user/images',
         params: {
+          page: this.page.currentPage,
+          per_page: this.page.pageSize,
           collect: this.activeName === 'collect' // 判断是否收藏true是收藏，false是全部
         }
       }).then(result => {
         this.list = result.data.results
+        this.page.total = result.data.total_count
       })
     }
   },
@@ -77,6 +114,9 @@ export default {
             }
             .img-item{
                 height: 150px;
+            }
+            i{
+              cursor: pointer;
             }
         }
     }
